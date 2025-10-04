8f368295d5cca760e393cceffcaab9a026de13ce
Mikael54 <mikael.minten1@gmail.com>
2025-10-04 20:32:56 +0100

    Testing experimental branch

---
+++ week1/code/boilerplate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Author: Mikael Minten mikael.minten25@imprial.ac.uk
+# Desc: simple boilerplate for shell scripts
+# Argument: none
+# Date: Sep 2025
+
+echo -e "\nThis is a shell script! \n"
+
+#exit
