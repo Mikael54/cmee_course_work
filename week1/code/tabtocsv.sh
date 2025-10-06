62bc32615633284853b7f00ecc0c9cab3fdf617d
Mikael54 <mikael.minten1@gmail.com>
2025-10-06 16:41:27 +0100

    Created tabtocsv.sh

--- week1/code/tabtocsv.sh
+++ week1/code/tabtocsv.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Author: Mikael Ridza Minten mikael.minten25@imperial.ac.uk
-# Script: tabtocsv.sh
-# Description: substitute the tabs in the files with commas
+# Script: csvtospace
+# Description: substitute the comma in the files with spaces
 #
-# Saves the output into a .csv file
-# Arguments: 1 -> tab delimited file
+# Saves the output into a .txt file
+# Arguments: 1 -> csv file
 # Date: Oct 2019
 
 if [$# -eq 0]; then
@@ -12,13 +12,16 @@ if [$# -eq 0]; then
     exit 1
 fi
 
-if grep -q $'\t' $1; then
-    echo "This file has no tabs"
+if grep -q $',' $1; then
+    echo "This file has no commas"
     exit 1
-fi # note- does a file with just one value contain tabs?- maybe this test is bad- do double check!!
+fi # note- does a file with just one value contain commas?- maybe this test is bad- do double check!!
 
 
-echo "Creating a comma delimited version of $1 ..."
+echo "Creating a space separated version of $1 ..."
 cat $1 | tr -s "\t" "," >> $1.csv
+cat $1 | tr -s "," " " >> $1.txt
 echo "Done!"
 exit
+
+# another idea- maybe write the script so it removes the .csv before changing it to txt?
