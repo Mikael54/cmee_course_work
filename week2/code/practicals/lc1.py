45ebc39e425c6deb2479eaed13f6ff7ad0ffad6b
Mikael54 <mikael.minten1@gmail.com>
2025-10-13 14:36:24 +0100

    Working through python practical lc1.

---
+++ week2/code/practicals/lc1.py
@@ -0,0 +1,36 @@
+birds = ( ('Passerculus sandwichensis','Savannah sparrow',18.7),
+          ('Delichon urbica','House martin',19),
+          ('Junco phaeonotus','Yellow-eyed junco',19.5),
+          ('Junco hyemalis','Dark-eyed junco',19.6),
+          ('Tachycineata bicolor','Tree swallow',20.2),
+         )
+
+#(1) Write three separate list comprehensions that create three different
+# lists containing the latin names, common names and mean body masses for
+# each species in birds, respectively. 
+
+latin_names = [row[0] for row in birds]
+common_names = [row[1] for row in birds]
+weights = [row[2] for row in birds]
+
+
+# (2) Now do the same using conventional loops (you can choose to do this 
+# before 1 !). 
+
+latin_names = []
+common_names = []
+weights = []
+
+for row in birds:
+    latin_names.append(row[0])
+    common_names.append(row[1])
+    weights.append(row[2])
+
+
+
+# A nice example out out is:
+# Step #1:
+# Latin names:
+# ['Passerculus sandwichensis', 'Delichon urbica', 'Junco phaeonotus', 'Junco hyemalis', 'Tachycineata bicolor']
+# ... etc.
+ 
