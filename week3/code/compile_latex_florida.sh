9cba9bc1bcb8e5e039b4f585b0747b67878f288d
Mikael54 <mikael.minten1@gmail.com>
2025-10-30 20:23:49 +0000

    making the latex document for florida

---
+++ week3/code/compile_latex_florida.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Compile in current directory but move PDF to results
+pdflatex ../data/florida_text.tex
+pdflatex ../data/florida_text.tex
+pdflatex ../data/florida_text.tex
+
+# Move PDF to results directory
+mv florida_text.pdf ../results/
+
+# Open PDF from results directory
+evince ../results/florida_text.pdf &
+
+## Cleanup
+rm -f florida_text.{aux,log,bbl,blg}
