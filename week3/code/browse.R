d351ce68da5d67738da0ec342d84669d542b6d95
Mikael54 <mikael.minten1@gmail.com>
2025-12-07 23:15:26 +0000

    Creating the browse script.

---
+++ week3/code/browse.R
@@ -0,0 +1,20 @@
+# Author: Mikael Minten
+# Date: October 2025
+# Script: control_flow.R
+# Description: illustrate the use of browser function.  
+
+Exponential <- function(N0 = 1, r = 1, generations = 10) {
+  # Runs a simulation of exponential growth
+  # Returns a vector of length generations
+  
+  N <- rep(NA, generations)    # Creates a vector of NA
+  
+  N[1] <- N0
+  for (t in 2:generations) {
+    N[t] <- N[t-1] * exp(r)
+    browser()
+  }
+  return (N)
+}
+
+plot(Exponential(), type="l", main="Exponential growth")
